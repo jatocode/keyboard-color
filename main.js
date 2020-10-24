a4d2da16b9f6f27a21ebf92693b9a548c9306aaa
Tobias Jansson <tobias.jansson@gmail.com>
2020-10-24 16:17:42 +0200

    Added arrow and other keys

--- main.js
+++ main.js
@@ -1,26 +1,55 @@
 import Keyboard from 'simple-keyboard';
 import 'simple-keyboard/build/css/index.css';
 
-let keyboard = new Keyboard({
-  onChange: input => onChange(input),
-  onKeyPress: button => onKeyPress(button),
-  layout: {
-    default: [
-        "Esc F1 F2 F3 F4 F5 F6 F6 F8 F9 F10 F11 F12",
-        "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
-        "{tab} q w e r t y u i o p [ ] \\",
-        "{lock} a s d f g h j k l ; ' {enter}",
-        "{shift} z x c v b n m , . / {shift}",
-        ".com @ {space}"
-    ]
+// Swedish layout from: https://github.com/simple-keyboard/simple-keyboard-layouts
+const main = new Keyboard('main',{
+    onChange: input => onChange(input),
+    onKeyPress: button => onKeyPress(button),
+    layout: {
+        default: [
+            "Esc F1 F2 F3 F4 F5 F6 F6 F8 F9 F10 F11 F12 PrintScr ScrLk Pause",
+            "\u00A7 1 2 3 4 5 6 7 8 9 0 + \u00B4 {bksp}",
+            "{tab} q w e r t y u i o p \u00E5 ¨",
+            "{lock} a s d f g h j k l \u00F6 \u00E4 ' {enter}",
+            "{shift} < z x c v b n m , . - {shift}",
+            ".com @ {space}"
+        ]
     }
 });
 
-function onChange(input){
-  document.querySelector(".input").value = input;
-  console.log("Input changed", input);
+const arrows = new Keyboard('arrows',{
+    onChange: input => onChange(input),
+    onKeyPress: button => onKeyPress(button),
+    layout: {
+        default: [
+            "{up}",
+            "{left} {down} {right}"
+        ]
+    },
+    display: {
+        '{up}':"^",
+        '{down}':"v",
+        '{left}':"<",
+        '{right}':">"
+    }
+});
+
+const other = new Keyboard('other',{
+    onChange: input => onChange(input),
+    onKeyPress: button => onKeyPress(button),
+    layout: {
+        default: [
+            "Ins Home PgUp",
+            "Del End PgDn"
+        ]
+    }
+});
+
+function onChange(input) {
+    document.querySelector(".input").value = input;
+    console.log("Input changed", input);
 }
 
-function onKeyPress(button){
-  console.log("Button pressed", button);
+function onKeyPress(button) {
+    console.log("Button pressed", button);
 }
