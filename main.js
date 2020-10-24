a628fbb6a60f29444d745cd2be8c601fe86e30d6
Tobias Jansson <tobias.jansson@gmail.com>
2020-10-24 17:08:09 +0200

    Toggle button colors

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 import Keyboard from 'simple-keyboard';
 import 'simple-keyboard/build/css/index.css';
 
+let keyboards = [];
+
 // Swedish layout from: https://github.com/simple-keyboard/simple-keyboard-layouts
 const main = new Keyboard('main', {
     onChange: input => onChange(input),
-    onKeyPress: button => onKeyPress(button),
+    onKeyPress: button => onKeyPress('main', button),
     layout: {
         default: [
-            "Esc F1 F2 F3 F4 F5 F6 F6 F8 F9 F10 F11 F12 PrintScr ScrLk Pause",
+            "Esc F1 F2 F3 F4 F5 F6 F7 F8 F9 F10 F11 F12 PrintScr ScrLk Pause",
             "\u00A7 1 2 3 4 5 6 7 8 9 0 + \u00B4 {bksp}",
             "{tab} q w e r t y u i o p \u00E5 ¨",
             "{lock} a s d f g h j k l \u00F6 \u00E4 ' {enter}",
@@ -22,10 +24,11 @@ const main = new Keyboard('main', {
         }
     ]
 });
+keyboards.push(main);
 
 const arrows = new Keyboard('arrows', {
     onChange: input => onChange(input),
-    onKeyPress: button => onKeyPress(button),
+    onKeyPress: button => onKeyPress('arrows', button),
     layout: {
         default: [
             "{up}",
@@ -39,10 +42,11 @@ const arrows = new Keyboard('arrows', {
         '{right}': ">"
     }
 });
+keyboards.push(arrows);
 
 const other = new Keyboard('other', {
     onChange: input => onChange(input),
-    onKeyPress: button => onKeyPress(button),
+    onKeyPress: button => onKeyPress('other', button),
     layout: {
         default: [
             "Ins Home PgUp",
@@ -50,12 +54,20 @@ const other = new Keyboard('other', {
         ]
     }
 });
+keyboards.push(other);
 
 function onChange(input) {
     document.querySelector(".input").value = input;
     console.log("Input changed", input);
 }
 
-function onKeyPress(button) {
+function onKeyPress(keyboard, button) {
     console.log("Button pressed", button);
+    swapColor(keyboard, button, 'color1');
+}
+
+function swapColor(keyboardName, button, className ) {
+    const keyboard = keyboards.find(k => k.currentInstanceName == keyboardName);
+    const el = keyboard.getButtonElement(button);
+    el.classList.toggle(className);
 }
