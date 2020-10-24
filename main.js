023dc958ace3591ae7e804c425a23cfc709d3f67
Tobias Jansson <tobias.jansson@gmail.com>
2020-10-24 15:07:31 +0200

    First

---
+++ main.js
@@ -0,0 +1,16 @@
+import Keyboard from 'simple-keyboard';
+import 'simple-keyboard/build/css/index.css';
+
+let keyboard = new Keyboard({
+  onChange: input => onChange(input),
+  onKeyPress: button => onKeyPress(button)
+});
+
+function onChange(input){
+  document.querySelector(".input").value = input;
+  console.log("Input changed", input);
+}
+
+function onKeyPress(button){
+  console.log("Button pressed", button);
+}
